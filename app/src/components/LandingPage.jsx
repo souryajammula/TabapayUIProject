8d20842cf5de28d414ff31e082fcc115aa22181c
Sourya Jammula <souryaprateek2000@gmail.com>
2024-04-12 11:40:47 -0700

    First Draft Commit

--- app/src/components/LandingPage.jsx
+++ app/src/components/LandingPage.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-// import TreeView from "@material-ui/lab/TreeView";
-// import TreeItem from "@material-ui/lab/TreeItem";
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
-// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { Link } from "react-router-dom";
 
 const getTreeItemsFromData = (treeItems) => {
   return treeItems.map((treeItemData) => {
@@ -15,17 +10,9 @@ const getTreeItemsFromData = (treeItems) => {
     if (treeItemData.children && treeItemData.children.length > 0) {
       children = getTreeItemsFromData(treeItemData.children);
     }
-    if (treeItemData.route) {
-      return (
-        <Link to={treeItemData.route} key={treeItemData.id}>
-          <TreeItem nodeId={treeItemData.id} label={treeItemData.name} key={treeItemData.id}>
-            {children}
-          </TreeItem>
-        </Link>
-      );
-    }
+    
     return (
-      <TreeItem nodeId={treeItemData.id} label={treeItemData.name} key={treeItemData.id}>
+      <TreeItem nodeId={treeItemData.id} label={treeItemData.name} itemId={treeItemData.id}>
         {children}
       </TreeItem>
     );
